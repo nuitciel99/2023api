52871cdb6f12bece207a9a13c0866ac3a2a5993e
jyk <nuitciel99@gmail.com>
2023-11-27 15:52:44 +0900

    loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const keyNum = "84b67f0a-5968-42bb-bdfc-4269e9cb92fc";
+  const currentPage = 1;
+  const listCnt = 10;
+  const [userData, setUserData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const callApi = async (currentPage)=>{
+    try {
+      setLoading(true)
+
+      const response = await axios.get(`http://api.kcisa.kr/openapi/API_CNV_060/request?serviceKey=${keyNum}&numOfRows=${listCnt}&pageNo=${currentPage}`)
+      setUserData(response.data.response.body.items.item)
+    } catch (error) {
+      console.error("error title : " + error);
+    }finally{
+      setLoading(false)
+    }
+    // axios.get(`http://api.kcisa.kr/openapi/API_CNV_060/request?serviceKey=${keyNum}&numOfRows=${listCnt}&pageNo=${currentPage}`)
+    //       .then((response)=>{
+    //         console.log(response);
+    //         setUserData(response.data.response.body.items.item)
+    //       })
+    //       .catch((error)=>{
+    //         console.log(error);
+    //       })
+  }
+
+  useEffect(()=>{
+    callApi(currentPage)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>recommendation of tour API </h1>
+      {
+        loading ? (<div>loading...</div>) : 
+        (
+          <ul>
+            {
+              userData.map((item, i)=>{
+                return(
+                  <li key={i}>{item.title}</li>
+                )
+              })
+            }
+          </ul>
+        )
+      }
     </div>
   );
 }
